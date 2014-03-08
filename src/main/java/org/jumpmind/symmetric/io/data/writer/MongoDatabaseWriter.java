fd099b555f219202f7157a614ba22b5ce574fc76
Chris Henson <chenson42@gmail.com>
2014-03-08 16:50:29 -0500

    Added support for running mongo commands using the sql event type in symmetricds

--- src/main/java/org/jumpmind/symmetric/io/data/writer/MongoDatabaseWriter.java
+++ src/main/java/org/jumpmind/symmetric/io/data/writer/MongoDatabaseWriter.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.jumpmind.symmetric.SymmetricException;
 import org.jumpmind.symmetric.io.data.CsvData;
 
+import com.mongodb.CommandResult;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
@@ -43,8 +44,6 @@ public class MongoDatabaseWriter extends AbstractDatabaseWriter {
      * how do we determine when to insert certain types because there is no
      * schema in mongo db. One idea I had was to cache create xml for tables
      * somewhere in mongodb and use it to determine types from the source.
-     * 
-     * TODO record load time
      */
 
     protected IMongoClientManager clientManager;
@@ -132,7 +131,17 @@ public class MongoDatabaseWriter extends AbstractDatabaseWriter {
 
     @Override
     protected boolean sql(CsvData data) {
-        return false;
+        statistics.get(batch).startTimer(DataWriterStatisticConstants.DATABASEMILLIS);
+        try {
+            DB db = clientManager.getDB(objectMapper.mapToDatabase(this.targetTable));
+            String command = data.getParsedData(CsvData.ROW_DATA)[0];
+            log.info("About to run command: {}", command);
+            CommandResult results = db.command(command);
+            log.info("The results of the command were: {}", results);
+        } finally {
+            statistics.get(batch).stopTimer(DataWriterStatisticConstants.DATABASEMILLIS);
+        }
+        return true;
     }
 
 }
